b0e99b8b08add8ad6420e9a754259cce6366e414
Emad <7454511+emadmahdi@users.noreply.github.com>
2026-08-08 18:00:07 -0400

    commit from cli

--- _deploy_github.bat
+++ _deploy_github.bat
@@ -1,5 +1,5 @@
 
-cd /d C:\DEVS\__github_plugin.video.arabicvideos
+cd /d E:\EMAD\Development\AV\__github_plugin.video.arabicvideos
 
 git add . && git commit -m "commit from cli" && git push -u origin main
 
